c57a3097426765de4cdd4ec8601a8f9a6eaa288d
Triantafyllos Paschaleris <pashaler@hotmail.com>
2015-07-29 02:24:06 +0300

    rewritten: fetch more data, better data management
    a code rewrite in order to get data for second sub-url from each page
    and organize data through Dictionary

--- evf_fetch.py
+++ evf_fetch.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
+__author__ = 'Paschaleris Triantafyllos'
 from multiprocessing.dummy import Pool as ThreadPool
 from bs4 import BeautifulSoup
 import urllib.request
+from collections import OrderedDict
 
 
 #Construct the URLs List
@@ -13,8 +15,9 @@ for i in range(1,pages):#next urls
 
 #Write the first line in the csv
 with open('fetched_data.csv',mode='w',encoding="utf-8") as f:
-    firstLineValuesNames = "Name of the Organization|EVS/PIC No.|Field|City|County|Website"#csv delimiter="|"
+    firstLineValuesNames = "Name|Address|Postal Code|City|Country|EVS accreditation type|Website|Email|Phone|Fax|EVS No.|PIC No.|Organisation topics"#csv delimiter="|"
     f.write(firstLineValuesNames + '\n')#write the first line
+
 
 
 
@@ -24,8 +27,79 @@ def getText(element):#In order to get the inner Text from the given element. Ret
     return ''
 
 
+def trimWhiteSpacesBeforeAfterDelimiter(stringg,delimiter):
+    return stringg.replace(' ' + delimiter, '|').replace(delimiter + ' ', '|')
+
+
+
+def getFullowedUrlData(furl,dataDict_passed):
+    print('fetching data for followed url: ' + furl)
+    req = urllib.request.Request(furl)
+    resp = urllib.request.urlopen(req)
+    respData = resp.read()
+    soup = BeautifulSoup(respData,from_encoding='utf-8')
+
+
+    dataDict = dataDict_passed#some renaming matching to the parent function's Dictionary naming
+
+    topics = soup.find('div', {'class' : 'topics'})
+    block1 = topics.find('div', {'class' : 'block1'})
+
+    if block1:#checking if NoneType
+        block1circle = block1.find('ul', {'class' : 'circle'})
+        if block1circle:#checking if NoneType
+            dataDict['Organisation topics']  = ' & '.join([elem.text for elem in block1circle.findAll('li')])#reading the data about Organisation's Type and Field (this order)
+
+
+
+    block2 = topics.find('div', {'class' : 'block2'})
+
+    for row,divElem in enumerate(block2.findAll('p')):#reading the data about Organisation's Type and Field (this order)
+        if "Web" in getText(divElem):
+            dataDict['Website'] = getText(divElem).replace('Web: ', '')
+        elif "Email" in getText(divElem):
+            dataDict['Email'] = getText(divElem).replace('Email: ', '')
+        elif "Phone" in getText(divElem):
+            dataDict['Phone'] = getText(divElem).replace('Phone: ', '')
+        elif "Fax" in getText(divElem):
+            dataDict['Fax'] = getText(divElem).replace('Fax: ', '')
+        elif row == 0:
+            dataDict['Name'] = getText(divElem)
+        elif row == 1:
+            dataDict['Address'] = getText(divElem)
+        elif row == 2:
+            dataDict['Postal Code'] = getText(divElem)
+        elif row == 3:
+            splitted = getText(divElem).split(',')#they are separated by ',' e.g.: "City, Country"
+            if len(splitted)>1:#sometimes City is not provided!
+                dataDict['City'] = splitted[0]
+                dataDict['Country'] = splitted[1]
+            else:
+                dataDict['Country'] = splitted[0]
+
+    return dataDict
+
+
+
 # The main Function fetching the data from given URL
 def getUrlData(url):
+
+    #Setup the dictionary that will hold the data
+    dataDict = OrderedDict()#...because the order is important
+    dataDict['Name']                    = ''#completed in followed url
+    dataDict['Address']                 = ''#completed in followed url
+    dataDict['Postal Code']             = ''#completed in followed url
+    dataDict['City']                    = ''#completed in followed url
+    dataDict['Country']                 = ''#completed in followed url
+    dataDict['EVS accreditation type']  = ''
+    dataDict['Website']                 = ''#completed in followed url
+    dataDict['Email']                   = ''#completed in followed url
+    dataDict['Phone']                   = ''#completed in followed url
+    dataDict['Fax']                     = ''#completed in followed url
+    dataDict['EVS No.']                 = ''
+    dataDict['PIC No.']                 = ''
+    dataDict['Organisation topics']     = ''#completed in followed url
+
 
     print('fetching data for url: ' + url)
     req = urllib.request.Request(url)
@@ -34,32 +108,32 @@ def getUrlData(url):
     soup = BeautifulSoup(respData,from_encoding='utf-8')
 
 
-    pagedata = []#init the returned list
+    pagedata = []#init the returned list that will hold the Dictionary for the
+
+
     for o_list in soup.findAll('div', {'class' : 'o_list'}):
-        org_name = getText(o_list.find('div', {'class': 'org_name'}))#get the organisation's name
-        #print(org_name.encode('utf-8'))
 
+        org_name = o_list.find('div', {'class': 'org_name'})#get the organisation's name
+        urlToFollow = r'https://europa.eu' + org_name.find('a')['href']#get the href attribute
+
+        # the following line will rewrite the dataDict with the data from the followe url
+        dataDict = getFullowedUrlData(furl=urlToFollow, dataDict_passed=dataDict)
+
+        # now fetch the remaining data values from the original parent-page
         two_columns = o_list.find('div', {'class': 'two columns'})#get the EVS divs
-        divElemList = []
         for divElement in two_columns.findAll('div'):#iter through the above divs
-            elementText =  getText(divElement).encode('utf-8')
-            if 'EVS accredited'.encode('utf-8') not in elementText:#we don't want the first div containing "EVS accredited". Only the next two containing the Number and the Status (in order)
-                #print(elementText)
-                divElemList.append(getText(divElement).replace("EVS no: ", "").replace("PIC no: ", "").replace(", ", "/"))
-        evsPicField = '|'.join(divElemList)
+            elementText =  getText(divElement)
+            if 'EVS no:' in elementText:
+                dataDict['EVS No.'] = elementText
+            elif 'PIC no:' in elementText:
+                dataDict['PIC No.'] = elementText
+            elif 'EVS accredited' not in elementText:
+                dataDict['EVS accreditation type'] = elementText.replace(", ", "/")
 
-        org_address = getText(o_list.find('div', {'class': 'org_address'}))#get the organisation's address (it will be "City,Country")
 
-        org_address = org_address.replace(",","|")#replacing the "," with the defined csv delimiter="|"
-        #print(org_address.encode('utf-8'))
+        lineToWrite = '|'.join(list(dataDict.values()))
 
-        org_website_link = getText(o_list.find('div', {'class': 'org_website_link'}))#get the organisation's website url
-        #print(org_website_link.encode('utf-8'))
-
-        variablesList = [org_name,evsPicField,org_address,org_website_link]
-
-        lineToWrite = '|'.join(variablesList)
-        pagedata.append(lineToWrite.replace('| ', '|') + '\n')#Trim the excess spaces and add new line character in each line
+        pagedata.append(trimWhiteSpacesBeforeAfterDelimiter(stringg=lineToWrite, delimiter='|') + '\n')#Trim the excess spaces and add new line character in each line
     return pagedata
 
 
