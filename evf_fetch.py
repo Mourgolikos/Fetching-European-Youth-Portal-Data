6a2c34ff7506cb4e62e19b9b7302448222e8924b
Triantafyllos Paschaleris <pashaler@hotmail.com>
2015-07-28 15:37:36 +0300

    some code cleanup
    changed output to csv with delimiter="|"

--- evf_fetch.py
+++ evf_fetch.py
@@ -9,42 +9,37 @@ for i in range(1,pages):#next urls
     urls.append('https://europa.eu/youth/evs_database_en?country=&town=&name=&pic=&eiref=&field_eyp_vp_accreditation_type=All&topic=&inclusion_topic=&page='+str(i))
 
 
+with open('fetched_data.csv',mode='w',encoding="utf-8") as f:#write the first line in the csv
+    firstLineValuesNames = "Name of the Organization,EVS/PIC No.,Field,City,County,Website"
+    f.write(firstLineValuesNames + '\n')#write the first line
+
+
 for url in urls:
     print('fetching data for url: ' + url)
     req = urllib.request.Request(url)
     resp = urllib.request.urlopen(req)
     respData = resp.read()
-
-
     soup = BeautifulSoup(respData,from_encoding="utf-8")
 
 
-    with open('fetched_data.txt',mode='a',encoding="utf-8") as f:
-        f.write("\n\n" + "#"*10 + "\nData from URL: " + url + "#"*10)
+    with open('fetched_data.csv',mode='a',encoding="utf-8") as f:#start writing data to csv delimiter="|"
         for o_list in soup.findAll('div', {'class' : 'o_list'}):
-            for org_name in o_list.findAll('div', {'class': 'org_name'}):
-                print(org_name.text.encode('utf-8'))
-                f.write(org_name.text + ",")
-            for org_address in o_list.findAll('div', {'class': 'org_address'}):
-                print(org_address.text.encode('utf-8'))
-                f.write(org_address.text + ",")
-            for org_eiref in o_list.findAll('div', {'class': 'org_eiref'}):
-                print(org_eiref.text.encode('utf-8'))
-                f.write(org_eiref.text + ",")
-            for org_website_link in o_list.findAll('div', {'class': 'org_website_link'}):
-                print(org_website_link.text.encode('utf-8'))
-                f.write(org_website_link.text + ",")
-            f.write("\n")
-
-
-    #data written from columns (all data)
-    with open('fetched_data_col.txt',mode='a',encoding="utf-8") as f:
-        f.write("\n\n" + "#"*10 + "\nData from URL: " + url + "#"*10)
-        for o_list in soup.findAll('div', {'class' : 'o_list'}):
-            for org_name in o_list.findAll('div', {'class': 'four columns'}):#this will fetch the first and third columns
-                print(org_name.text.encode('utf-8'))
-                f.write(org_name.text + " ||| ")
-            for org_address in o_list.findAll('div', {'class': 'two columns'}):#and now fetching the second column
-                print(org_address.text.encode('utf-8'))
-                f.write(org_address.text + " ||| ")
+            for org_name in o_list.findAll('div', {'class': 'org_name'}):#get the organisation's name
+                #print(org_name.text.encode('utf-8'))
+                f.write(org_name.text + "|")
+            for two_columns in o_list.findAll('div', {'class': 'two columns'}):#get the EVS divs
+                for divElement in two_columns.findAll('div'):#iter through the above divs
+                    elementText =  divElement.text.encode('utf-8')
+                    if 'EVS accredited'.encode('utf-8') not in elementText:#we don't want the first div containing "EVS accredited". Only the next two containing the Number and the Status (in order)
+                        #print(elementText)
+                        f.write(divElement.text.replace("EVS no: ","").replace("PIC no: ","").replace(", ","/") + "|")
+            for org_address in o_list.findAll('div', {'class': 'org_address'}):#get the organisation's address (it will be "City,Country")
+                #print(org_address.text.encode('utf-8'))
+                f.write(org_address.text.replace(",","|") + "|")#replacing the "," with the defined csv delimiter="|"
+            for org_eiref in o_list.findAll('div', {'class': 'org_eiref'}):#get the organisation's EIRef
+                #print(org_eiref.text.encode('utf-8'))
+                f.write(org_eiref.text + "|")
+            for org_website_link in o_list.findAll('div', {'class': 'org_website_link'}):#get the organisation's website url
+                #print(org_website_link.text.encode('utf-8'))
+                f.write(org_website_link.text)
             f.write("\n")
